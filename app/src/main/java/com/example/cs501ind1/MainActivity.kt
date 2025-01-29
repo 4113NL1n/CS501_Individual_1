be55a15a667b7d41e0d4fb7f069e97df2b5ead00
Allen Lin <allenlin@bu.edu>
2025-01-29 12:50:55 -0500

    Hello World for assignment 1 individual

---
+++ app/src/main/java/com/example/cs501ind1/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.cs501ind1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.cs501ind1.ui.theme.CS501Ind1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CS501Ind1Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "World",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier.fillMaxSize().padding(top =150.dp, bottom = 150.dp, end = 150.dp, start = 150.dp)
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CS501Ind1Theme {
+        Greeting("Android")
+    }
+}
